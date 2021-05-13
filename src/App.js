3a153d82808b133e3619c13a99ed5242f79f0656
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-05-12 19:37:49 -0700

    Forgot to make any commits. Completed project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header/Header";
+import Photo from "./components/POTD/POTD";
+
+const apod_url = `https://api.nasa.gov/planetary/apod?api_key=2ck2z3EFkCQivTfwdMlszYlGHyMoHaTaVKFvd7jH`;
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(apod_url)
+      .then(response => console.log("HERE IS THE RESPONSE: ", response) || response.data)
+      .catch(error => console.log("HERE IS THE ERROR: ", error))
+
+      setData(result)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <Photo data={data} />
+      <h3>More on this Photo</h3>
+      <div>
+        <p>{data.explanation}</p>
+      </div>
     </div>
   );
 }
